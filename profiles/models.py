642fb93babfaf51d29cb5cd0d3263b21f06d1de3
axy-moon <augxy666@gmail.com>
2022-12-07 18:49:00 +0530

    user registration init

--- profiles/models.py
+++ profiles/models.py
@@ -1,10 +1,41 @@
 from django.db import models
 from django.contrib.auth.base_user import AbstractBaseUser
+from django.db.models.base import Model
+from django.db.models.deletion import CASCADE, SET_DEFAULT
+from django.db.models.fields import AutoField, CharField, IntegerField
+from django.db.models.fields.related import ForeignKey
+from django.utils import timezone
+from datetime import date
 
 
 # Create your models here.
 
-class User(AbstractBaseUser):
-    rollno = models.CharField(max_length=7,null=False)
 
-class profile
+""" 
+class profile(models.Model):
+    firstname = models.ForeignKey(User,on_delete=CASCADE)
+    lastname = models.ForeignKey(User,on_delete=CASCADE)
+    pro_pic = models.ImageField()
+    group = models.ForeignKey(User,on_delete=CASCADE)
+    year = models.DateField()
+    bio = models.TextField()
+
+class Posts(models.Model):
+    title = models.CharField(max_length=100,null=False)
+    content = models.TextField()
+    time_posted = models.DateTimeField()
+    author = models.ForeignKey(User,on_delete=CASCADE)
+    image = models.ImageField()
+    comments = models.TextField()
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(User,on_delete=CASCADE)
+    reciever = models.ForeignKey(User,on_delete=CASCADE)
+    timestamp = models.DateTimeField()
+    message = models.TextField()
+
+class Tokens(models.Model):
+    name = models.CharField(max_length=50)
+    roll_no = models.CharField(max_length=7)
+    year = models.DateField() """
--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User,auth
 # Create your views here.
 
 def profile(request):
@@ -10,9 +10,18 @@ def login(request):
 
 def register(request):
     if request.method == 'POST':
-        username = requset.POST[]
-        
-    return render(request,'register.html')
+        roll_no = request.POST['rollnum']
+        username = request.POST['uname']
+        firstname = request.POST['fname'] 
+        lastname = request.POST['lname']
+        passwd = request.POST['pswd']
+        email = request.POST['email']
+
+        user = User.objects.create_user(username=username,password=passwd,first_name=firstname,last_name=lastname,email=email)
+        user.save()
+        print("User Created")
+    else:
+        return render(request,'register.html')
 
 def feed(request):
     return render(request,'feed.html')
