1d126131401fc4bd752433abfb44010f7896e64e
axy-moon <augxy666@gmail.com>
2022-12-10 00:24:17 +0530

    user reg and login

--- mx_community/mca/models.py
+++ mx_community/mca/models.py
@@ -3,5 +3,4 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class NewUser(AbstractUser):
-    rollno = models.CharField(max_length=7,null=False)
-    
+    rollno = models.CharField(max_length=7,null=False,unique=True)
--- mx_community/mca/urls.py
+++ mx_community/mca/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('',views.feed,name='feed'),
     path('register',views.register,name='register'),    
-    #path('login',views.login,name='login'),
+    path('login',views.login,name='login'),
     #path('feed',views.feed,name='feed'),
     #path('messages',views.messages,name='messages'),
     #path('search',views.search,name='search'),
